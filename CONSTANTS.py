60052e574199f99770585e09264a5864d5760381
ZekeWeng <ZekeWengP@gmail.com>
2024-11-28 23:38:53 -0500

    removed antiquated files

--- CONSTANTS.py
+++
@@ -1,8 +0,0 @@
-### PATHS
-INPUT = "data/raw"
-OUTPUT = "data/outputs"
-
-GAMES = f"{INPUT}/games.csv"
-PLAYERS = f"{INPUT}/players.csv"
-PLAYS = f"{INPUT}/plays.csv"
-PLAYER_PLAY = f"{INPUT}/player_play.csv"
